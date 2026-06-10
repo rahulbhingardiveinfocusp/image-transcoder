714d76fc5f6a139bf750d9ff7700831f2035cfd6
Rahul Bhingardive <rahul.bhingardive@amycus.infocusp.com>
2026-06-10 16:04:01 +0530

    fix

--- backend/app/services/s3_service.py
+++ backend/app/services/s3_service.py
@@ -3,17 +3,20 @@ import logging
 from app.core.config import settings
 
 logger = logging.getLogger(__name__)
-endpoint_url = settings.LOCALSTACK_ENDPOINT or None
+if settings.LOCALSTACK_ENDPOINT:
+    endpoint_url = settings.LOCALSTACK_ENDPOINT
+else:
+    endpoint_url = f"https://s3.{settings.AWS_REGION}.amazonaws.com"
 class S3Service:
     def __init__(self):
 
         self.s3 = boto3.client(
             "s3",
-            endpoint_url=endpoint_url, # Changed to use your config
             region_name=settings.AWS_REGION,
+            endpoint_url=endpoint_url,
             aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
             aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-            config=boto3.session.Config(signature_version='s3v4')
+            config=boto3.session.Config(signature_version="s3v4"),
         )
     def generate_presigned_url(
         self,
