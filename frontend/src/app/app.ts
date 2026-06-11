f5fcfa275c982409ce24ef8bef4f8030493f9822
Rahul Bhingardive <rahul.bhingardive@amycus.infocusp.com>
2026-06-11 17:07:02 +0530

    adde variables

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -5,7 +5,7 @@ import { environment } from '../environments/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   selectedFile: File | null = null;
@@ -28,36 +28,34 @@ export class App {
     const file = this.selectedFile;
 
     // 1. Get presigned URL
-    this.http.post<any>(
-      `${this.apiBaseUrl}/images/request-upload`,
-      { filename: file.name , content_type: file.type}
-    ).subscribe({
-      next: (res) => {
-         const uploadUrl = res.upload_url;
+    this.http
+      .post<any>(`${this.apiBaseUrl}/images/request-upload`, {
+        filename: file.name,
+        content_type: file.type,
+      })
+      .subscribe({
+        next: (res) => {
+          const uploadUrl = res.upload_url;
 
-      // 2. Upload to S3 (MUST match signed headers exactly)
-        fetch(uploadUrl, {
-          method: 'PUT',
-          body: file,
-          headers: {
-            'Content-Type': file.type   // 🔥 MUST MATCH presigned URL
-          }
-        })
-        .then((res) => {
-          if (!res.ok) {
-            throw new Error(`Upload failed: ${res.status}`);
-          }
-          alert('Upload successful!');
-          this.selectedFile = null;
-        })
-        .catch((err) => {
-          console.error('S3 Upload failed:', err);
-        });
-      },
-      error: (err) => {
-        console.error('Backend failed:', err);
-        alert('Failed to get upload URL from server.');
-      }
-    });
+          // IMPORTANT: MUST use headers in PUT request
+          const headers = new HttpHeaders({
+            'Content-Type': file.type,
+          });
+          // 2. Upload directly to S3
+          this.http.put(uploadUrl, file, { headers }).subscribe({
+            next: () => {
+              alert('Upload successful!');
+              this.selectedFile = null;
+            },
+            error: (err) => {
+              console.error('S3 Upload failed:', err);
+            },
+          });
+        },
+        error: (err) => {
+          console.error('Backend failed:', err);
+          alert('Failed to get upload URL from server.');
+        },
+      });
   }
 }
