0a061e89f03e6504e052023754df05d76058e3f4
Rahul Bhingardive <rahul.bhingardive@amycus.infocusp.com>
2026-06-11 13:59:12 +0530

    fix

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment';
 
 @Component({
@@ -27,27 +27,29 @@ export class App {
 
     const file = this.selectedFile;
 
-    // 1. Get presigned URL
+    // 1. Get presigned URL from backend
     this.http.post<any>(
       `${this.apiBaseUrl}/images/request-upload`,
-      { filename: file.name}
+      { filename: file.name }
     ).subscribe({
-      next: (res) => {
+      next: async (res) => {
         const uploadUrl = res.upload_url;
 
-        // IMPORTANT: MUST use headers in PUT request
+        try {
+          // 2. Upload directly to S3 (NO HEADERS)
+          await fetch(uploadUrl, {
+            method: 'PUT',
+            body: file
+          });
 
-        // 2. Upload directly to S3
-        this.http.put(uploadUrl, file).subscribe({
-          next: () => {
-            alert('Upload successful!');
-            this.selectedFile = null;
-          },
-          error: (err) => {
-            console.error('S3 Upload failed:', err);
-          }
-        });
+          alert('Upload successful!');
+          this.selectedFile = null;
+
+        } catch (err) {
+          console.error('S3 Upload failed:', err);
+        }
       },
+
       error: (err) => {
         console.error('Backend failed:', err);
         alert('Failed to get upload URL from server.');
