0ed529af15fbe4a45a90eb45ffd116c46705c030
Rahul Bhingardive <rahul.bhingardive@amycus.infocusp.com>
2026-06-11 13:19:44 +0530

    fix

--- backend/app/tasks/celery_app.py
+++ backend/app/tasks/celery_app.py
@@ -5,7 +5,7 @@ celery_app = Celery("worker")
 
 celery_app.conf.update(
     broker_url="sqs://",
-    task_default_queue=settings.SQS_QUEUE_NAME,
+    task_default_queue=settings.SQS_QUEUE_URL,
     broker_transport_options={
         "region": settings.AWS_REGION,
         "visibility_timeout": 3600,
