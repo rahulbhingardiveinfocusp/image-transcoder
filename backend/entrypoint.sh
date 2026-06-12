a793e2389700652ba3d190a4432dd69c95340cbe
Rahul Bhingardive <rahul.bhingardive@amycus.infocusp.com>
2026-06-12 11:52:15 +0530

    adde variables

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -10,7 +10,9 @@ else
     echo "Running database migrations..."
     alembic upgrade head
 
+    echo "Starting Python bridge..."
+    python -u bridge.py &
+
     echo "Starting FastAPI..."
     exec uvicorn app.main:app --host 0.0.0.0 --port 8000
-    python bridge.py
 fi
