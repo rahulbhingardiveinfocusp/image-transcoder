9ca31d26c805dd7a19656369967d8a02c0428cdf
Rahul Bhingardive <rahul.bhingardive@amycus.infocusp.com>
2026-06-17 13:49:38 +0530

    error handling and edge cased

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,7 +53,7 @@ provider "aws" {
 # =========================================================================
 resource "aws_s3_bucket" "app_bucket" { 
   bucket        = var.s3_bucket_name 
-  force_destroy = false
+  force_destroy = true
 }
 
 resource "aws_s3_bucket_cors_configuration" "app_bucket_cors" {
