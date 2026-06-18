048697975d93df28d3daab18c4a319d0e6e1a624
Rahul Bhingardive <rahul.bhingardive@amycus.infocusp.com>
2026-06-18 11:49:54 +0530

    added table

--- backend/app/api/v1/router.py
+++ backend/app/api/v1/router.py
@@ -14,6 +14,13 @@ router = APIRouter()
 class UploadRequest(BaseModel):
     filename: str
     content_type:str
+
+    
+@router.post("/request-upload")
+async def request_upload(payload: UploadRequest, db: AsyncSession = Depends(get_db)):
+    result = await ImageService.get_upload_url(db, payload.filename, payload.content_type)
+    return result
+
 class ImageResponse(BaseModel):
     id: UUID
     filename: str
@@ -25,12 +32,6 @@ class ImageResponse(BaseModel):
         "from_attributes": True
     }
     
-@router.post("/request-upload")
-async def request_upload(payload: UploadRequest, db: AsyncSession = Depends(get_db)):
-    result = await ImageService.get_upload_url(db, payload.filename, payload.content_type)
-    return result
-
-
 @router.get("/images", response_model=list[ImageResponse],)
 async def get_all_images(db: AsyncSession = Depends(get_db)):
     return await ImageService.get_all_images(db)
