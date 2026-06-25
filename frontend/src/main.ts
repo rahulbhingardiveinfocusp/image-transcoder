56c0c6b8c60e3a4d95f6b7ac9fa030398758a1a8
Rahul Bhingardive <rahul.bhingardive@amycus.infocusp.com>
2026-06-25 13:30:27 +0530

    admin dashboard

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,14 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
-
-
-import { routes } from './app/app.routes';
+import { appConfig, awsConfig } from './app/app.config';
 import { App } from './app/app';
+import { Amplify } from 'aws-amplify';
 
-bootstrapApplication(App, {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient()
-  ]
-}).catch((err) => console.error(err));
+Amplify.configure(awsConfig);
+bootstrapApplication(App, appConfig)
+  .catch((err) => console.error(err));
